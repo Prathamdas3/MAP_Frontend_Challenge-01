0d2061c4c5deaef428fb2917fd43aea8bba9d280
Pratham Das <prathamdas003@gmail.com>
2024-06-29 02:04:16 +0530

    dependences update

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import path from 'path'
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 
 const manifestForPlugIn: Partial<VitePWAOptions> = {
-  registerType: 'autoUpdate',
-  includeAssets: [],
-  // devOptions: {
-  //   enabled: true,
-  // },
+  registerType: 'prompt',
+  devOptions: {
+    enabled: true,
+  },
   manifest: {
     theme_color: '#ffffff',
     background_color: '#ffffff',
@@ -24,25 +23,23 @@ const manifestForPlugIn: Partial<VitePWAOptions> = {
         src: './public/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'muskable',
       },
       {
         src: './public/icon-256x256.png',
         sizes: '256x256',
         type: 'image/png',
-        purpose: 'muskable',
       },
       {
         src: './public/icon-384x384.png',
         sizes: '384x384',
         type: 'image/png',
-        purpose: 'any',
+        purpose: 'any maskable',
       },
       {
         src: './public/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any',
+        purpose: ' maskable',
       },
     ],
   },
