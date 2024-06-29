3413367e612e4918520bd5a50b3d880632a463fd
Pratham Das <prathamdas003@gmail.com>
2024-06-30 01:53:35 +0530

    refactor(app): pwa images

--- vite.config.ts
+++ vite.config.ts
@@ -24,24 +24,24 @@ const manifestForPlugIn: Partial<VitePWAOptions> = {
     orientation: 'portrait',
     icons: [
       {
-        src: './public/icon-192x192.png',
+        src: '/icon-192x192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'favicon',
       },
       {
-        src: './public/icon-256x256.png',
+        src: '/icon-256x256.png',
         sizes: '256x256',
         type: 'image/png',
       },
       {
-        src: './public/icon-384x384.png',
+        src: '/icon-384x384.png',
         sizes: '384x384',
         type: 'image/png',
         purpose: 'any maskable',
       },
       {
-        src: './public/icon-512x512.png',
+        src: '/icon-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: ' maskable',
